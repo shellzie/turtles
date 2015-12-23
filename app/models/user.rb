bea9a44c44a106813009a24d4e08c6fbbc69b374
mkam <michellekam@gmail.com>
2015-12-22 20:47:00 -0800

    trying to add user successfully via rails console

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true,  length: { minimum: 8, maximum: 15 }
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+end
---
+++ db/migrate/20151222225203_change_password_to_users.rb
@@ -0,0 +1,5 @@
+class ChangePasswordToUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :password
+  end
+end
