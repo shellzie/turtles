57cd9dab1d16df3c8f14109183fa654ecd26cb14
dev <michellekam@gmail.com>
2016-11-16 15:53:49 -0700

    Adding cors method back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::Base
 
   # protect_from_forgery with: :exception
 
-  # before_filter :cors
+  before_filter :cors
 
   include SessionsHelper
 
   #TODO: should have a specific domain listed here rather than allowing *
-  # def cors
-  #   headers["Access-Control-Allow-Origin"]  = "*"
-  #   headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-  #   headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-  #   head(:ok) if request.request_method == "OPTIONS"
-  # end
+  def cors
+    headers["Access-Control-Allow-Origin"]  = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+    head(:ok) if request.request_method == "OPTIONS"
+  end
+
 end
