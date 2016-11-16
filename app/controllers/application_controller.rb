fb7664925326ac42e82d8bb2d15f92e0006ff321
dev <michellekam@gmail.com>
2016-11-16 12:17:24 -0700

    adding requestheader for cors method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-
   # protect_from_forgery with: :exception
 
-  before_filter :cor
+  before_filter :cors
 
   include SessionsHelper
 
   #TODO: should have a specific domain listed here rather than allowing *
-  def cor
-    headers["Access-Control-Allow-Origin"]  = "*"
-    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
-    headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-    head(:ok) if request.request_method == "OPTIONS"
+  def cors
+    headers["Access-Control-Allow-Origin"]  = '*'
+    headers["Access-Control-Allow-Methods"] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
