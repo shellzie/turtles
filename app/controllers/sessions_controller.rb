3da5d947456a4d54de04fbe8982689ec54a95235
dev <michellekam@gmail.com>
2017-01-18 13:06:57 -0700

    added JWT for mobile side. authentication of user upon login is working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,15 +17,23 @@ class SessionsController < ApplicationController
 
   #separate login method for mobile since params are not stored in params[:sessions] they are stored
   # in params[:email] and also we don't have to use 'remember me' cookie since xcode
-  #remembers user via UserPreferences structure
+  #remembers user via UserPreferences structure. Using a JWT to check if mobile user is authenticated.
+
+
   def login
-    logger.debug "++++++++++ in login fun"
-    user = User.find_by(email: params[:email].downcase)
+
+    email_downcase = params[:email].downcase
+    user = User.find_by email: email_downcase
     if user && user.authenticate(params[:password])
-      log_in user
-      render :nothing => true, :status => 200
+      authenticate_user(user)
+      # if user
+      #   log_in user
+      # else
+      #   render json:{ errors: ['Not Authenticated']}
+      # end
     else
-      render :nothing => true, :status => 404 #record not found
+      # render :nothing => true, :status => 404 #record not found
+      render nothing: true, json: {errors: ['Invalid Username/Password']}, status: :unauthorized
     end
   end
 
@@ -33,4 +41,20 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to login_url
   end
+
+  # when this method is called from login(), we assume existence of 'user'. (it is NOT null)
+  def authenticate_user(user)
+    render json: payload(user)
+  end
+
+  private
+
+  def payload(user)
+    return nil unless user and user.id
+    {
+        auth_token: JsonWebToken.encode({user_id: user.id}),
+        user: {id: user.id, email: user.email}
+    }
+  end
 end
+
